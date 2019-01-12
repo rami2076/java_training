4a4a24111b6fd255a73f0af9b7f6a41d25d10603
osusi-tomato <shiro.noire33@gmail.com>
2019-01-13 08:57:07 +0900

    sync remote

---
+++ src/Activity1.java
@@ -0,0 +1,12 @@
+public class Activity1 {
+
+
+    //Hello Worldを出力してみよう！
+    //10行目のSystem.out.printlnでコンソールに文字を表示できます。
+    //コンソールに文字を出力することを標準出力といいます。
+    //標準出力はよく出てくるので覚えておきましょう！
+    public static void main(String[] args) {
+        //ここに文字をいれるをHello World!に変えてみよう！
+        System.out.println("ここに文字をいれる。");
+    }
+}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-
-    //Hello Worldを出力してみよう！
-    public static void main(String[] args) {
-        System.out.println("ここに文字をいれる。");
-    }
-}
